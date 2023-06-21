e6b3f3d2850d987c93a356756318c830939e2bbf
Tim Heilman <tdh@stanfordalumni.org>
2023-06-21 13:59:02 -0700

    build: ch4.01 no functions needed in this stack, comment it out

--- serverless.ts
+++ serverless.ts
@@ -1,6 +1,6 @@
 import type { AWS } from '@serverless/typescript';
 
-import hello from '@functions/hello';
+// import hello from '@functions/hello';
 
 const serverlessConfiguration: AWS = {
   org: 'theilman',
@@ -22,7 +22,7 @@ const serverlessConfiguration: AWS = {
     },
   },
   // import the function via paths
-  functions: { hello },
+  // functions: { hello }, // SCOR-12 no functions needed in ch4.01
   package: { individually: true },
   custom: {
     esbuild: {
