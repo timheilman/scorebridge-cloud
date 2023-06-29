640f2b73a4dc14019824719e02878f1be4c751e6
Tim Heilman <tdh@stanfordalumni.org>
2023-06-29 16:57:39 -0700

    refactor: SCOR-18 ch4.07 refactor just-written script

--- ts-node-scripts/stackOutputsForDotenv.ts
+++ ts-node-scripts/stackOutputsForDotenv.ts
@@ -4,6 +4,35 @@ function camelToScreamingSnake(str) {
   return str.replace(/([a-z])([A-Z])/g, "$1_$2").toUpperCase();
 }
 
+function linesPastStackOutputPrompt(slsInfoLines: string[]) {
+  return slsInfoLines.slice(
+    Math.min(
+      slsInfoLines.length,
+      slsInfoLines.findIndex((s) => s.match(/^Stack Outputs:/)) + 1
+    )
+  );
+}
+
+function stackOutputLines(postStackOutputPromptLines: string[]) {
+  return postStackOutputPromptLines.slice(
+    0,
+    Math.min(
+      postStackOutputPromptLines.length,
+      postStackOutputPromptLines.findIndex((s) => s.match(/^[^ ]/))
+    )
+  );
+}
+
+function shellInputFormat(myStackOutputLines: string[]) {
+  return myStackOutputLines.map((stackOutputLine) => {
+    const colonSpace = ": ";
+    const keyAndRest = stackOutputLine.split(colonSpace);
+    return `${camelToScreamingSnake(keyAndRest[0].trim())}="${keyAndRest
+      .slice(1)
+      .join(colonSpace)}"`;
+  });
+}
+
 exec("npx sls info --verbose", (error, stdout /* , stderr */) => {
   if (error) {
     console.log(
@@ -13,28 +42,9 @@ exec("npx sls info --verbose", (error, stdout /* , stderr */) => {
   }
   // stderr expected and ignored
   const slsInfoLines = stdout.split("\n");
-  const postStackOutputPromptLines = slsInfoLines.slice(
-    Math.min(
-      slsInfoLines.length,
-      slsInfoLines.findIndex((s) => s.match(/^Stack Outputs:/)) + 1
-    )
-  );
   console.log(
-    postStackOutputPromptLines
-      .slice(
-        0,
-        Math.min(
-          postStackOutputPromptLines.length,
-          postStackOutputPromptLines.findIndex((s) => s.match(/^[^ ]/))
-        )
-      )
-      .map((stackOutputLine) => {
-        const colonSpace = ": ";
-        const keyAndRest = stackOutputLine.split(colonSpace);
-        return `${camelToScreamingSnake(keyAndRest[0].trim())}="${keyAndRest
-          .slice(1)
-          .join(colonSpace)}"`;
-      })
-      .join("\n")
+    shellInputFormat(
+      stackOutputLines(linesPastStackOutputPrompt(slsInfoLines))
+    ).join("\n")
   );
 });
