f258d1e2cddf93ce53316d85a052e1d1a17b368d
Tim Heilman <tdh@stanfordalumni.org>
2023-07-06 07:08:49 -0700

    build: SCOR-18 fix array index safety issues and wrong
    missing newline for npm run -- exportEnvDev

--- ts-node-scripts/stackOutputsForDotenv.ts
+++ ts-node-scripts/stackOutputsForDotenv.ts
@@ -4,23 +4,32 @@ function camelToScreamingSnake(str) {
   return str.replace(/([a-z])([A-Z])/g, "$1_$2").toUpperCase();
 }
 
-function linesPastStackOutputPrompt(slsInfoLines: string[]) {
-  return slsInfoLines.slice(
-    Math.min(
-      slsInfoLines.length,
-      slsInfoLines.findIndex((s) => s.match(/^Stack Outputs:/)) + 1
-    )
-  );
+function findIndexes(array, condition) {
+  return array
+    .map((element, index) => (condition(element) ? index : -1))
+    .filter((index) => index !== -1);
 }
 
-function stackOutputLines(postStackOutputPromptLines: string[]) {
-  return postStackOutputPromptLines.slice(
-    0,
-    Math.min(
-      postStackOutputPromptLines.length,
-      postStackOutputPromptLines.findIndex((s) => s.match(/^[^ ]/))
-    )
-  );
+function stackOutputsPromptIndex(slsInfoLines) {
+  const i = slsInfoLines.findIndex((s) => s.match(/^Stack Outputs:/));
+  if (i === -1) {
+    throw new Error("Could not find prompt 'Stack Outputs:'");
+  }
+  return i;
+}
+
+function firstBlankIndexAfterPrompt(slsInfoLines: string[], startIndex) {
+  const blankLineIndexes = findIndexes(slsInfoLines, (l) => l.match(/^[^ ]/));
+  return blankLineIndexes.find((n) => n >= startIndex);
+}
+
+function linesBetweenPromptAndEmptyLine(slsInfoLines: string[]) {
+  const startIndex = stackOutputsPromptIndex(slsInfoLines) + 1;
+  const endIndex = firstBlankIndexAfterPrompt(slsInfoLines, startIndex);
+  if (endIndex === -1) {
+    return slsInfoLines.slice(startIndex);
+  }
+  return slsInfoLines.slice(startIndex, endIndex);
 }
 
 function shellInputFormat(myStackOutputLines: string[]) {
@@ -43,8 +52,6 @@ exec("npx sls info --verbose", (error, stdout /* , stderr */) => {
   // stderr expected and ignored
   const slsInfoLines = stdout.split("\n");
   console.log(
-    shellInputFormat(
-      stackOutputLines(linesPastStackOutputPrompt(slsInfoLines))
-    ).join("\n")
+    shellInputFormat(linesBetweenPromptAndEmptyLine(slsInfoLines)).join("\n")
   );
 });
