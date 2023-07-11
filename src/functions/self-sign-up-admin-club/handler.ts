6a37d4cd85bebad2ec7173204b75bee7f31b1418
Tim Heilman <tdh@stanfordalumni.org>
2023-07-11 11:03:48 -0700

    wip: SCOR-6 placeholder files for new self-sign-up fn

---
+++ src/functions/self-sign-up-admin-club/handler.ts
@@ -0,0 +1,44 @@
+import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
+import Chance from "chance";
+import { fromEnv } from "@aws-sdk/credential-providers";
+import { PostConfirmationTriggerEvent } from "aws-lambda";
+import fromSsoUsingProfileFromEnv from "../../libs/from-sso-using-profile-from-env";
+
+const chance = new Chance();
+
+// eslint-disable-next-line import/prefer-default-export
+export const main = async (event: PostConfirmationTriggerEvent) => {
+  const { USERS_TABLE } = process.env;
+  if (event.triggerSource !== "PostConfirmation_ConfirmSignUp") {
+    return event;
+  }
+  const { name } = event.request.userAttributes;
+  const suffix = chance.string({
+    length: 8,
+    casing: "upper",
+    alpha: true,
+    numeric: true,
+  });
+  const screenName = `${name.replace(/[^a-zA-Z0-9]/g, "")}${suffix}`;
+  const user = {
+    id: { S: event.userName },
+    name: { S: name },
+    screenName: { S: screenName },
+    createdAt: { S: new Date().toJSON() },
+  };
+  const client = new DynamoDBClient({
+    region: process.env.AWS_REGION || "NO_REGION_FOUND_IN_ENV",
+    credentials:
+      process.env.NODE_ENV === "test"
+        ? fromSsoUsingProfileFromEnv()
+        : fromEnv(),
+  });
+  const command = new PutItemCommand({
+    TableName: USERS_TABLE,
+    Item: user,
+    ConditionExpression: "attribute_not_exists(id)",
+  });
+  await client.send(command);
+
+  return event;
+};
---
+++ src/functions/self-sign-up-admin-club/index.ts
@@ -0,0 +1,19 @@
+import { handlerPath } from "@libs/handler-resolver";
+
+export default {
+  handler: `${handlerPath(__dirname)}/handler.main`,
+  environment: {
+    USERS_TABLE: {
+      Ref: "UsersTable",
+    },
+  },
+  iamRoleStatements: [
+    {
+      Effect: "Allow",
+      Action: "dynamodb:PutItem",
+      Resource: {
+        "Fn::GetAtt": ["UsersTable", "Arn"],
+      },
+    },
+  ],
+};
