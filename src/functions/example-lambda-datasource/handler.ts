a2afe97f7245d8bfc17258b65893da26e92d8dc6
Tim Heilman <tdh@stanfordalumni.org>
2023-07-11 11:23:19 -0700

    build: update hello world lambda to discover type
    of the appsync resolver event

--- src/functions/example-lambda-datasource/handler.ts
+++ src/functions/example-lambda-datasource/handler.ts
@@ -2,7 +2,11 @@ import requiredEnvVar from "../../libs/requiredEnvVar";
 
 // eslint-disable-next-line import/prefer-default-export,no-unused-vars
 export async function main(event) {
-  return `Hello World!  region: ${requiredEnvVar(
-    "AWS_REGION"
-  )}, args: ${JSON.stringify(event.arguments)}`;
+  console.log(
+    `Hello World!  region: ${requiredEnvVar(
+      "AWS_REGION"
+    )}, args: ${JSON.stringify(event.arguments)}`
+  );
+  console.log(JSON.stringify(event, null, 2));
+  return event;
 }
