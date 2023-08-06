6bb75eaeef195fa94329608f07248380262dfe85
Tim Heilman <tdh@stanfordalumni.org>
2023-08-06 16:14:58 -0700

    build: provide scripts to refresh details to the webapp.

---
+++ ts-node-scripts/provideEnvEntriesToWebapp.ts
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+function convertToReactActVar(s: string) {
+  return `REACT_APP_${s}=${process.env[s]}`;
+}
+
+[
+  "AWS_REGION",
+  "ADD_CLUB_API_KEY",
+  "COGNITO_USER_POOL_ID",
+  "COGNITO_USER_POOL_CLIENT_ID_WEB",
+  "API_URL",
+].forEach((webappDependentVar) => {
+  console.log(convertToReactActVar(webappDependentVar));
+});
