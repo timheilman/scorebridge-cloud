94aa01fc891d5b16bce8c96ea624969f438d49db
Tim Heilman <tdh@stanfordalumni.org>
2023-08-10 17:18:06 -0700

    fix: hopefully to use one error middleware regardless of error type

--- src/libs/lambda.ts
+++ src/libs/lambda.ts
@@ -1,5 +1,3 @@
-import { InputValidationError } from "@libs/errors/input-validation-error";
-import { UserAlreadyExistsError } from "@libs/errors/user-already-exists-error";
 import middy from "@middy/core";
 import middyJsonBodyParser from "@middy/http-json-body-parser";
 import { Handler } from "aws-lambda";
@@ -7,17 +5,17 @@ import { Handler } from "aws-lambda";
 export const middyJson = (handler: Handler) =>
   middy(handler).use(middyJsonBodyParser());
 
-function errorMiddleware(errorClass) {
+function errorMiddleware() {
   return {
     onError: (request: { error: Error; response: unknown }) => {
-      if (request.error instanceof errorClass) {
+      if (request.error instanceof Error) {
         // the response vtl template handles this case
         // where the response is { error: { message, type } }
         request.response = {
           error: {
             message: request.error.message,
             // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-assignment
-            type: errorClass.name,
+            type: request.error.constructor.name,
           },
         };
 
@@ -28,8 +26,4 @@ function errorMiddleware(errorClass) {
 }
 
 export const middyWithErrorHandling = (handler: Handler) =>
-  middy(handler)
-    .use(errorMiddleware(Error))
-    .use(errorMiddleware(UserAlreadyExistsError))
-    .use(errorMiddleware(InputValidationError));
-// to get most specific, list from general to specific...
+  middy(handler).use(errorMiddleware());
