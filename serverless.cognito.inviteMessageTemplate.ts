3c16ea4dca7948344a373b1e595de2deacc8ad83
Tim Heilman <tdh@stanfordalumni.org>
2023-08-14 11:10:22 -0700

    feat: mention why they are getting the email

--- serverless.cognito.inviteMessageTemplate.ts
+++ serverless.cognito.inviteMessageTemplate.ts
@@ -5,7 +5,10 @@ function readFileSync(filePath: string): string {
   return fs.readFileSync(filePath, "utf8");
 }
 const templateContext = readFileSync(
-  path.join(__dirname, "serverless.cognito.inviteMessageTemplate.handlebars"),
+  path.join(
+    __dirname,
+    "serverless.cognito.inviteMessageTemplate.html.cognito.hbs",
+  ),
 );
 const template = Handlebars.compile(templateContext);
 
