252e5c0434de4825dfe9986636d2242c40b8259e
Tim Heilman <tdh@stanfordalumni.org>
2023-08-13 19:13:41 -0700

    fix: typo in html for email invite

--- serverless.cognito.ts
+++ serverless.cognito.ts
@@ -3,7 +3,7 @@ const head = `<head><title>email title</title></head>`;
 const heading = `<h2>Welcome to the ScoreBridge-\${sls:stage} Portal</h2>`;
 const list = `<ul><li>Username: {username}</li><li>Password: {####}</li></ul>`;
 const footer = `<hr/><a href="https://localhost:3000/">https://localhost:3000/</a>`;
-const info = `<p>Please use these credentials to <a href="http://localhost:3000/"/>login</a>:</p>${list}${footer}`;
+const info = `<p>Please use these credentials to <a href="http://localhost:3000/">login</a>:</p>${list}${footer}`;
 const body = `<body>${heading}${info}</body>`;
 const inviteMessageTemplate = `<html>${head}${body}</html>`;
 export default {
