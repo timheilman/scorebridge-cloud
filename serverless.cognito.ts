e0389771ed42b94b62f56d773e53e8e64a381097
Tim Heilman <tdh@stanfordalumni.org>
2023-08-14 10:41:14 -0700

    refactor: extract invitation email template to own file

--- serverless.cognito.ts
+++ serverless.cognito.ts
@@ -1,11 +1,5 @@
-// import { AppSyncConfig } from "serverless-appsync-plugin/src/types/plugin"
-const head = `<head><title>email title</title></head>`;
-const heading = `<h2>Welcome to the ScoreBridge-\${sls:stage} Portal</h2>`;
-const list = `<ul><li>Username: {username}</li><li>Password: {####}</li></ul>`;
-const footer = `<hr/><a href="https://localhost:3000/">https://localhost:3000/</a>`;
-const info = `<p>Please use these credentials to <a href="http://localhost:3000/">login</a>:</p>${list}${footer}`;
-const body = `<body>${heading}${info}</body>`;
-const inviteMessageTemplate = `<html>${head}${body}</html>`;
+import { inviteMessageTemplate } from "./serverless.cognito.inviteMessageTemplate";
+
 export default {
   CognitoUserPool: {
     Type: "AWS::Cognito::UserPool",
