ab34239d1553b97aa5220cd63b03274b48e5123b
Tim Heilman <tdh@stanfordalumni.org>
2023-08-14 11:02:31 -0700

    refactor: introduce handlebars and put invite email template in it

--- serverless.cognito.inviteMessageTemplate.ts
+++ serverless.cognito.inviteMessageTemplate.ts
@@ -1,7 +1,43 @@
-const head = `<head><title>email title</title></head>`;
-const heading = `<h2>Welcome to the ScoreBridge-\${sls:stage} Portal</h2>`;
-const list = `<ul><li>Username: {username}</li><li>Password: {####}</li></ul>`;
-const footer = `<hr/><a href="https://localhost:3000/">https://localhost:3000/</a>`;
-const info = `<p>Please use these credentials to <a href="http://localhost:3000/">login</a>:</p>${list}${footer}`;
-const body = `<body>${heading}${info}</body>`;
-export const inviteMessageTemplate = `<html>${head}${body}</html>`;
+import fs from "fs";
+import Handlebars from "handlebars";
+import path from "path";
+async function readFile(filePath: string): Promise<string> {
+  return new Promise<string>((resolve, reject) => {
+    fs.readFile(filePath, "utf8", (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+}
+const templateContent = readFile(
+  path.join(__dirname, "serverless.cognito.inviteMessageTemplate.handlebars"),
+);
+const template = Handlebars.compile(templateContent);
+
+function context(stage: string) {
+  if (stage === "dev") {
+    return {
+      portalName: "ScoreBridge-dev Admin Portal",
+      loginUrl: "https://dev.d2efhllh5f21k3.amplifyapp.com/",
+    };
+  } else if (stage === "staging") {
+    return {
+      portalName: "ScoreBridge-staging Admin Portal",
+      loginUrl: "https://todo.get.staging.set.up.amplifyapp.com/",
+    };
+  } else if (stage === "prod") {
+    return {
+      portalName: "ScoreBridge-staging Admin Portal",
+      loginUrl: "https://todo.get.prod.set.up.amplifyapp.com/",
+    };
+  } else {
+    throw new Error(`Unrecognized stage ${stage}`);
+  }
+}
+
+export function inviteMessageTemplate(stage: string) {
+  return template(context(stage));
+}
