e943500d60c06071e72fdef4a68d260e8b7b8e77
Tim Heilman <tdh@stanfordalumni.org>
2023-08-24 13:41:17 -0700

    build: another try

--- src/mapping-templates-ts/Query.exampleLambdaDataSource.ts
+++ src/mapping-templates-ts/Query.exampleLambdaDataSource.ts
@@ -1,19 +1,30 @@
 import { Context } from "@aws-appsync/utils";
 import { LambdaRequest } from "@aws-appsync/utils/lib/resolver-return-types";
 
-import { QueryExampleLambdaDataSourceArgs } from "../../appsync";
+import {
+  ExampleLambdaDataSourceOutput,
+  QueryExampleLambdaDataSourceArgs,
+} from "../../appsync";
 
-export const request = function <ARGS>(ctx: Context<ARGS>): LambdaRequest {
+export const request = (
+  ctx: Context<QueryExampleLambdaDataSourceArgs>,
+): LambdaRequest => {
   return {
     operation: "Invoke",
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     payload: ctx,
   };
-}<QueryExampleLambdaDataSourceArgs>;
+};
 
-export const response = function <ARGS, RESPONSE>(
-  ctx: Context<ARGS, object, object, object, RESPONSE>,
-) {
+export const response = (
+  ctx: Context<
+    QueryExampleLambdaDataSourceArgs,
+    object,
+    object,
+    object,
+    ExampleLambdaDataSourceOutput
+  >,
+) => {
   if (ctx.error) {
     util.error(ctx.error.message, ctx.error.type);
   }
