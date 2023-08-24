bddca4a9fc55ed6cd76a1040ccbc853a096963b3
Tim Heilman <tdh@stanfordalumni.org>
2023-08-24 09:22:26 -0700

    test: SCOR-89 exploratory try to get middy logged

--- src/libs/lambda.ts
+++ src/libs/lambda.ts
@@ -2,13 +2,18 @@ import middy from "@middy/core";
 import middyJsonBodyParser from "@middy/http-json-body-parser";
 import { Handler } from "aws-lambda";
 
+import { logFn } from "../libs/logging";
+const log = logFn("src.libs.lambda.");
+
 export const middyJson = (handler: Handler) =>
   middy(handler).use(middyJsonBodyParser());
 
 function errorMiddleware() {
   return {
     onError: (request: { error: Error; response: unknown }) => {
+      log("errorMiddleware.onError.start", "debug", { request });
       if (request.error instanceof Error) {
+        log("errorMiddleware.onError.instanceofError", "debug", { request });
         // the response vtl template handles this case
         // where the response is { error: { message, type } }
         request.response = {
@@ -19,8 +24,10 @@ function errorMiddleware() {
           },
         };
 
+        log("errorMiddleware.onError.resolving", "debug", { request });
         return Promise.resolve(request.response);
       }
+      log("errorMiddleware.onError.end", "debug", { request });
     },
   };
 }
