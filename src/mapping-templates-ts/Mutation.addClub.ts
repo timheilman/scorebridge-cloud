9ea456b7fe1b648d62afed011fc42588bca77652
Tim Heilman <tdh@stanfordalumni.org>
2023-08-24 09:06:53 -0700

    refactor: SCOR-89 inline one passthru response fn to debug

--- src/mapping-templates-ts/Mutation.addClub.ts
+++ src/mapping-templates-ts/Mutation.addClub.ts
@@ -1,12 +1,24 @@
-import { AddClubResponse, MutationAddClubArgs } from "../../appsync";
-import {
-  lambdaErrorHandlingResponse,
-  lambdaPassThroughInvoke,
-} from "./mappingTemplateUtils";
+import { Context, util } from "@aws-appsync/utils";
 
+import { AddClubResponse, MutationAddClubArgs } from "../../appsync";
+import { logFn } from "../libs/logging";
+import { lambdaPassThroughInvoke } from "./mappingTemplateUtils";
+const log = logFn("src.mapping-templates-ts.Mutation.addClub.");
 export const request = lambdaPassThroughInvoke<MutationAddClubArgs>;
 
-export const response = lambdaErrorHandlingResponse<
-  MutationAddClubArgs,
-  AddClubResponse
->;
+export const response = function <ARGS, RESPONSE>(
+  ctx: Context<
+    ARGS,
+    object,
+    object,
+    object,
+    RESPONSE & { error?: { message: string; type: string } }
+  >,
+) {
+  log("lambdaErrorHandlingResponse.start", "debug", { ctx });
+  if (ctx.result.error) {
+    util.error(`${ctx.result.error.message}`, `${ctx.result.error.type}`);
+  }
+
+  return ctx.result;
+}<MutationAddClubArgs, AddClubResponse>;
