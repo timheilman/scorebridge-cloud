514910629aa9495d493ce0630f4447abd2c590b8
Tim Heilman <tdh@stanfordalumni.org>
2023-08-24 15:15:50 -0700

    fix: SCOR-89 not being able to import in appsync_js ts resolvers
    is a real drag, I wonder if I can resolve it.  forgot to duplicate code across resolvers.

--- src/mapping-templates-ts/Mutation.unexpectedError.ts
+++ src/mapping-templates-ts/Mutation.unexpectedError.ts
@@ -17,6 +17,14 @@ export const response = (
   if (ctx.error) {
     util.error(ctx.error.message, ctx.error.type);
   }
+  // this happens due to middy error handling middleware per Yan Cui
+  /* eslint-disable @typescript-eslint/ban-ts-comment,@typescript-eslint/no-unsafe-argument,@typescript-eslint/no-unsafe-member-access */
+  // @ts-ignore
+  if (ctx.result.error) {
+    // @ts-ignore
+    util.error(ctx.result.error.message, ctx.result.error.type);
+  }
+  /* eslint-enable @typescript-eslint/ban-ts-comment,@typescript-eslint/no-unsafe-argument,@typescript-eslint/no-unsafe-member-access */
 
   return ctx.result;
 };
