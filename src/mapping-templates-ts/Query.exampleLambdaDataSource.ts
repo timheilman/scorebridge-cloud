cd9c46f9db5b3a588073185b945cb5fc7f2b079a
Tim Heilman <tdh@stanfordalumni.org>
2023-08-24 13:37:48 -0700

    build: SCOR-89 trying to inline the problem parts

--- src/mapping-templates-ts/Query.exampleLambdaDataSource.ts
+++ src/mapping-templates-ts/Query.exampleLambdaDataSource.ts
@@ -1,16 +1,22 @@
-import {
-  ExampleLambdaDataSourceOutput,
-  QueryExampleLambdaDataSourceArgs,
-} from "../../appsync";
-import {
-  lambdaErrorHandlingResponse,
-  lambdaPassThroughInvoke,
-} from "./mappingTemplateUtils";
+import { Context } from "@aws-appsync/utils";
+import { LambdaRequest } from "@aws-appsync/utils/lib/resolver-return-types";
 
-export const request =
-  lambdaPassThroughInvoke<QueryExampleLambdaDataSourceArgs>;
+import { QueryExampleLambdaDataSourceArgs } from "../../appsync";
 
-export const response = lambdaErrorHandlingResponse<
-  QueryExampleLambdaDataSourceArgs,
-  ExampleLambdaDataSourceOutput
->;
+export const request = function <ARGS>(ctx: Context<ARGS>): LambdaRequest {
+  return {
+    operation: "Invoke",
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    payload: ctx,
+  };
+}<QueryExampleLambdaDataSourceArgs>;
+
+export const response = function <ARGS, RESPONSE>(
+  ctx: Context<ARGS, object, object, object, RESPONSE>,
+) {
+  if (ctx.error) {
+    util.error(ctx.error.message, ctx.error.type);
+  }
+
+  return ctx.result;
+};
