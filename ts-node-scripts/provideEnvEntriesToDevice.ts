8d3384f35804da213e57de7f03556354c879c145
Tim Heilman <tdh@stanfordalumni.org>
2023-09-04 12:27:47 -0700

    build: provide scripts to refresh exported env to device repo too

---
+++ ts-node-scripts/provideEnvEntriesToDevice.ts
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+function convertToReactActVar(s: string) {
+  return `EXPO_PUBLIC_${s}=${process.env[s]}`;
+}
+
+[
+  "AWS_REGION",
+  "COGNITO_USER_POOL_ID",
+  "COGNITO_USER_POOL_CLIENT_ID_WEB",
+  "API_URL",
+  "PORTAL_URL",
+  "STAGE",
+].forEach((webappDependentVar) => {
+  console.log(convertToReactActVar(webappDependentVar));
+});
