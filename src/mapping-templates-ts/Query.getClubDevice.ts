38f9bfdff00bece88ce09d9254206b527f46bc14
Tim Heilman <tdh@stanfordalumni.org>
2023-09-08 17:40:32 -0700

    test: exploratory is the problem in the multitenancy

--- src/mapping-templates-ts/Query.getClubDevice.ts
+++ src/mapping-templates-ts/Query.getClubDevice.ts
@@ -1,14 +1,14 @@
 import { Context, DynamoDBGetItemRequest, util } from "@aws-appsync/utils";
 
 import { QueryGetClubDeviceArgs } from "../../scorebridge-ts-submodule/graphql/appsync";
-import { errorOnDeviceLevelMultitenancy } from "./mappingTemplateUtils";
+// import { errorOnDeviceLevelMultitenancy } from "./mappingTemplateUtils";
 
 export function request(
   ctx: Context<QueryGetClubDeviceArgs>,
 ): DynamoDBGetItemRequest {
   const clubId = ctx.arguments.clubId;
   const clubDeviceId = ctx.arguments.clubDeviceId;
-  errorOnDeviceLevelMultitenancy(ctx, clubId, clubDeviceId);
+  // errorOnDeviceLevelMultitenancy(ctx, clubId, clubDeviceId);
   return {
     operation: "GetItem",
     key: util.dynamodb.toMapValues({ clubId, clubDeviceId }),
