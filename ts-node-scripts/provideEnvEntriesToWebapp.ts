6ad419b3053994713e582c107040acfde87eb0d7
Tim Heilman <tdh@stanfordalumni.org>
2024-01-02 12:41:27 -0800

    build: SCOR-143 properly slurp cypress task env vars

--- ts-node-scripts/provideEnvEntriesToWebapp.ts
+++ ts-node-scripts/provideEnvEntriesToWebapp.ts
@@ -5,6 +5,10 @@ function convertToViteVar(s: string) {
   return `VITE_${s}=${process.env[s]}`;
 }
 
+function convertToCypressTaskVar(s: string) {
+  return `CYPRESS_TASK_${s}=${process.env[s]}`;
+}
+
 [
   "AWS_REGION",
   "CREATE_CLUB_API_KEY",
@@ -12,13 +16,18 @@ function convertToViteVar(s: string) {
   "COGNITO_USER_POOL_CLIENT_ID_WEB",
   "API_URL",
   "STAGE",
-].forEach((webappDependentVar) => {
-  console.log(convertToViteVar(webappDependentVar));
-});
+].forEach((webappDependentVar) =>
+  console.log(convertToViteVar(webappDependentVar)),
+);
+
+["COGNITO_USER_POOL_ID", "COGNITO_USER_POOL_CLIENT_ID_WEB"].forEach(
+  (cypressTaskDependentVar) => convertToCypressTaskVar(cypressTaskDependentVar),
+);
 // cannot automatically test captcha in prod
 if (process.env.STAGE !== "prod") {
   // this is the test pass-thru site key
   console.log(
+    // TODO: while this is technically not a secret, env shouldn't be used for it in prod; fix it
     `VITE_RECAPTCHA2_SITE_KEY=6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI`,
   );
 }
